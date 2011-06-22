731f502b3af291272808d6064f4b2b7cb461d6dc
mihai-trascau <mihai.trascau@gmail.com>
2011-06-22 17:51:52 +0300

    Change-Id: I506f3001e9234aea600f0456c5d43ae7a47506c4

--- src/java/env/GUI.java
+++ src/java/env/GUI.java
@@ -12,7 +12,7 @@ import javax.swing.*;
 import cartagoEnvironment.*;
 
 @SuppressWarnings("serial")
-public class GUI extends JFrame {
+public class GUI extends JFrame implements Runnable {
 	
 	private Map map;
 	private JLabel labelMap[][];
@@ -34,37 +34,56 @@ public class GUI extends JFrame {
 		
 		this.drawMap(null,null);
 		
+		
+		for(int i = 0; i < map.getHeigth(); i++)
+			for(int j = 0; j < map.getWidth(); j++)
+				this.mapPanel.add(labelMap[i][j]);
+		
+		mapPanel.validate();
+		mapPanel.repaint();
+		
 		this.getContentPane().add(mapPanel);
+		new Thread(this).start();
 		this.setVisible(true);
 	}
 	
-	public void drawMap(Hashtable<String,Position> agentPosition, Hashtable<String, AgentState> agentState) {
-		mapPanel.removeAll();
+	public synchronized void drawMap(Hashtable<String,Position> agentPosition, Hashtable<String, AgentState> agentState) {
+		//mapPanel.removeAll();
 		
 		for(int i = 0; i < map.getHeigth(); i++)
 			for(int j = 0; j < map.getWidth(); j++)
 			{
 				if(map.getPosition(i, j) == 0) {
-					labelMap[i][j] = new JLabel(i+","+j);
+					if(agentPosition == null)
+						labelMap[i][j] = new JLabel(i+","+j);
+					else {
+						labelMap[i][j].setIcon(null);
+						labelMap[i][j].setText(i+","+j);
+					}
 					labelMap[i][j].setOpaque(true);
 					labelMap[i][j].setBackground(Color.WHITE);
 					labelMap[i][j].setForeground(Color.LIGHT_GRAY);
 					labelMap[i][j].setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
 				}
 				else if(map.getPosition(i, j) == 1) {
-					labelMap[i][j] = new JLabel();
+					if(agentPosition == null)
+						labelMap[i][j] = new JLabel();
+					else
+						labelMap[i][j].setIcon(null);
 					labelMap[i][j].setOpaque(true);
 					labelMap[i][j].setBackground(Color.GRAY);
 					labelMap[i][j].setForeground(Color.LIGHT_GRAY);
 				}
 				else if(map.getPosition(i, j) == 2) {
-					labelMap[i][j] = new JLabel();
+					if(agentPosition == null)
+						labelMap[i][j] = new JLabel();
 					labelMap[i][j].setOpaque(true);
 					labelMap[i][j].setBackground(Color.GRAY);
 					labelMap[i][j].setIcon(getScaledIcon("res/img/crate.png", 0.17));
 				}
 				else if(map.getPosition(i, j) == 3) {
-					labelMap[i][j] = new JLabel();
+					if(agentPosition == null)
+						labelMap[i][j] = new JLabel();
 					labelMap[i][j].setOpaque(true);
 					labelMap[i][j].setBackground(Color.CYAN);
 					labelMap[i][j].setIcon(getScaledIcon("res/img/crate.png", 0.17));
@@ -73,6 +92,8 @@ public class GUI extends JFrame {
 				labelMap[i][j].setHorizontalAlignment(JLabel.CENTER);
 				labelMap[i][j].setVerticalTextPosition(JLabel.BOTTOM);
 				labelMap[i][j].setHorizontalTextPosition(JLabel.CENTER);
+				labelMap[i][j].validate();
+				labelMap[i][j].repaint();
 			}
 		
 		if(agentPosition != null) {
@@ -81,17 +102,54 @@ public class GUI extends JFrame {
 				int j = agentPosition.get(agentName).getY();
 				labelMap[i][j].setForeground(Color.RED);
 				labelMap[i][j].setText(agentName);
-				labelMap[i][j].setIcon(getScaledIcon("res/img/loaded_robot.png", 0.15));
+				AgentState state = agentState.get(agentName);
+				switch(state)
+				{
+				case CARRYING:
+					labelMap[i][j].setIcon(getScaledIcon("res/img/loaded_robot.png", 0.15));
+					break;
+				case IDLE:
+					labelMap[i][j].setIcon(getScaledIcon("res/img/idle_robot.png", 0.15));
+					break;
+				case LOADING:
+					labelMap[i][j].setIcon(getScaledIcon("res/img/loading_robot.png", 0.15));
+					break;
+				case UNLOADING:
+					labelMap[i][j].setIcon(getScaledIcon("res/img/unloading_robot.png", 0.15));
+					break;
+				case MOVING:
+					labelMap[i][j].setIcon(getScaledIcon("res/img/empty_robot.png", 0.15));
+					break;
+				case PLANNING:
+					labelMap[i][j].setIcon(getScaledIcon("res/img/empty_robot.png", 0.15));
+				}
 			}
 		}
 		
+		mapPanel.validate();
+		mapPanel.repaint();
+		
+		
+		/*
 		for(int i = 0; i < map.getHeigth(); i++)
 			for(int j = 0; j < map.getWidth(); j++)
 				this.mapPanel.add(labelMap[i][j]);
+		//*/
 		
-		this.setVisible(false);
-		this.setVisible(true);
-		//this.repaint();
+		/*
+		for(int i = 0; i < map.getHeigth(); i++)
+			for(int j = 0; j < map.getWidth(); j++) {
+				labelMap[i][j].validate();
+				labelMap[i][j].repaint();
+			}
+		
+		
+		mapPanel.validate();
+		mapPanel.repaint();
+		*/
+		//validate();
+		//repaint();
+		//*/
 	}
 	
 	private ImageIcon getScaledIcon(String path, double scaleFactor) {
@@ -123,4 +181,8 @@ public class GUI extends JFrame {
 		g.dispose();
 		return new ImageIcon(resizedImage);
 		}
+
+	public void run() {
+		
+	}
 }
